47976d10340d2b49c6d67a05e94d3ca46cb0ea03
DiogoRocha22 <diogorocha2906@gmail.com>
2024-03-07 23:21:42 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld title="Homem come maçã"
+    text="Na manhã desta madrugada, às 7 da tarde, um homem pega a maçã parte-a e a devora.."
+    author="Choquei"
+  />
+</template>
+
+<style scoped>
+  
+</style>
