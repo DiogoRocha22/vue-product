1109554006f686fba9b01be9dabc0182111af233
DiogoRocha22 <diogorocha2906@gmail.com>
2024-03-11 14:16:05 -0300

    validando cart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,9 @@
     import cart from '../assets/images/icon-cart.svg'
     import avatar from '../assets/images/image-avatar.png'
 
-    const itens = 1
+    import { ref } from 'vue';
+
+    const itens = ref(0)
 
 </script>
 
@@ -26,7 +28,7 @@
         <div class="flex gap-10 items-center">
             <div class="relative px-1">
                 <img class=" w-8 h-8" :src="cart" alt="">
-                <div class=" absolute bottom-7 right-0 bg-orange rounded-xl px-2 text-white text-xs">{{ itens }}</div>
+                <div v-if="itens && typeof itens == 'number'" class=" absolute bottom-7 right-0 bg-orange rounded-xl px-2 text-white text-xs">{{ itens }}</div>
             </div>
             <img class=" w-14" :src="avatar" alt="">
         </div>
